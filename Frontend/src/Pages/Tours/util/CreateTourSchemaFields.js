68995d8505c318c360de298fb208caff67fa478b
Tomerzhr <tomerzhr@gmail.com>
2024-10-28 16:55:43 +0200

    last

--- Frontend/src/Pages/Tours/util/CreateTourSchemaFields.js
+++ Frontend/src/Pages/Tours/util/CreateTourSchemaFields.js
@@ -13,7 +13,7 @@ export const schema = {
   image: {
     pattern: {
       value:
-        /^https?:\/\/(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}\/.*\.(?:png|jpg)|^www\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,}\/.*\.(?:png|jpg)$/,
+        /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
       message: "Invalid image URL.",
     },
   },
